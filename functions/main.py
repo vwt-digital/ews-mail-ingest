5cef359b223394a85a2ebe3d53d773c1cc8fb44e
dvanrhenen <d.vanrhenen@vwt.digital>
2020-02-12 14:01:33 +0100

    Added streaming upload to GCS. DAT-1876

--- functions/main.py
+++ functions/main.py
@@ -8,6 +8,8 @@ import datetime
 
 from exchangelib import Credentials, Account, Configuration, Folder, \
     FileAttachment, errors, Version, Build
+from google.auth.transport.requests import AuthorizedSession
+from google.resumable_media import requests, common
 from google.cloud import kms_v1, storage, pubsub_v1
 
 
@@ -22,23 +24,25 @@ def process_message_original(bucket, message, path):
 
 
 # Upload attachments to GCS storage
-def process_message_attachments(bucket, message, path):
+def process_message_attachments(client, bucket_name, message, path):
     message_attachments = []
     for attachment in message.attachments:
         if isinstance(attachment, FileAttachment):
-
-            clean_attachment_name = attachment.name.\
-                replace(' ', '_').\
-                replace('.', '_', attachment.name.count('.') - 1).\
+            clean_attachment_name = attachment.name. \
+                replace(' ', '_'). \
+                replace('.', '_', attachment.name.count('.') - 1). \
                 replace('-', '_')
 
             try:
-
                 file_path = '%s/%s' % (path, clean_attachment_name)
 
-                blob = bucket.blob(file_path)
-                blob.upload_from_string(attachment.content,
-                                        content_type=attachment.content_type)
+                with GCSObjectStreamUpload(
+                        client=client, bucket_name=bucket_name,
+                        blob_name=file_path) as f, attachment.fp as fp:
+                    buffer = fp.read(1024)
+                    while buffer:
+                        f.write(buffer)
+                        buffer = fp.read(1024)
 
                 message_attachments.append({
                     'name': clean_attachment_name,
@@ -195,8 +199,9 @@ def ews_to_bucket(request):
 
                 # Save message attachments to bucket
                 message_attachments = \
-                    process_message_attachments(bucket=bucket,
-                                                message=message, path=path)
+                    process_message_attachments(
+                        client=client, bucket_name=config.GCP_BUCKET_NAME,
+                        message=message, path=path)
 
                 # Mark message as 'read' and move to folder
                 process_message_status(account=account, message=message)
@@ -209,3 +214,78 @@ def ews_to_bucket(request):
                                      topic_name=topic_name, request=request)
 
                 logging.info('Finished processing of e-mail')
+
+
+class GCSObjectStreamUpload(object):
+    def __init__(
+            self,
+            client: storage.Client,
+            bucket_name: str,
+            blob_name: str,
+            chunk_size: int = 256 * 1024
+    ):
+        self._client = client
+        self._bucket = self._client.bucket(bucket_name)
+        self._blob = self._bucket.blob(blob_name)
+
+        self._buffer = b''
+        self._buffer_size = 0
+        self._chunk_size = chunk_size
+        self._read = 0
+
+        self._transport = AuthorizedSession(
+            credentials=self._client._credentials
+        )
+        self._request = None  # type: requests.ResumableUpload
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, *_):
+        if exc_type is None:
+            self.stop()
+
+    def start(self):
+        url = (
+            f'https://www.googleapis.com/upload/storage/v1/b/'
+            f'{self._bucket.name}/o?uploadType=resumable'
+        )
+        self._request = requests.ResumableUpload(
+            upload_url=url, chunk_size=self._chunk_size
+        )
+        self._request.initiate(
+            transport=self._transport,
+            content_type='application/octet-stream',
+            stream=self,
+            stream_final=False,
+            metadata={'name': self._blob.name},
+        )
+
+    def stop(self):
+        self._request.transmit_next_chunk(self._transport)
+
+    def write(self, data: bytes) -> int:
+        data_len = len(data)
+        self._buffer_size += data_len
+        self._buffer += data
+        del data
+        while self._buffer_size >= self._chunk_size:
+            try:
+                self._request.transmit_next_chunk(self._transport)
+            except common.InvalidResponse:
+                self._request.recover(self._transport)
+        return data_len
+
+    def read(self, chunk_size: int) -> bytes:
+        # I'm not good with efficient no-copy buffering so if this is
+        # wrong or there's a better way to do this let me know! :-)
+        to_read = min(chunk_size, self._buffer_size)
+        memview = memoryview(self._buffer)
+        self._buffer = memview[to_read:].tobytes()
+        self._read += to_read
+        self._buffer_size -= to_read
+        return memview[:to_read].tobytes()
+
+    def tell(self) -> int:
+        return self._read
