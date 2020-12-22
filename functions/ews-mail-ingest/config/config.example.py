a77f9b8bc65d4ca6bdc435a26c89c820a5effad4
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-12-22 09:21:43 +0100

    Remove obsolete cloud function. DAT-5076

--- functions/ews-mail-ingest/config/config.example.py
+++
@@ -1,9 +0,0 @@
-GCP_BUCKET_NAME = "bucket-name"
-EXCHANGE_URL = "https://exchange.com/"
-EXCHANGE_USERNAME = "username"
-EXCHANGE_VERSION = {
-    'major': 15,
-    'minor': 20
-}
-TOPIC_PROJECT_ID = "project-id"
-TOPIC_NAME = "topic-name"
