d050ea0732e3a3d46de6f0a7c078eaa46e4569df
Nico Griffioen <nicogriffioen@gmail.com>
2021-01-27 11:43:28 +0100

    Add test config file. DAT-5429

---
+++ functions/ews-mail-ingest/tests/config.tests.py
@@ -0,0 +1,17 @@
+EMAIL_ADDRESSES = {
+
+}
+
+EXCHANGE_URL = "https://outlook.office365.com/ews/exchange.asmx"
+EXCHANGE_VERSION = {
+    'major': 15,
+    'minor': 20
+}
+
+PROJECT_ID = None
+BUCKET_NAME = None
+TOPIC_NAME = None
+
+ERROR_EMAIL_ADDRESS = None
+
+ATTACHMENTS_TO_STORE = ['application/pdf']
