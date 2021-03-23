f07900881b67426ddaac7b6079d95f6fe328b74c
SpringPie <bente.vandepol@gmail.com>
2021-03-23 10:44:49 +0100

    SAST scan on GCP gave eslint errors DAT-6050

--- functions/ews-mail-ingest/publish.py
+++ functions/ews-mail-ingest/publish.py
@@ -1,13 +1,13 @@
 import json
 import logging
 
-from bleach import Cleaner, sanitizer
 from gobits import Gobits
 from google.cloud.pubsub_v1 import PublisherClient
 from requests import Request
 
-from config import ATTACHMENTS_TO_STORE, ALLOWED_HTML_BODY_TAGS
-from mail import Email, Attachment
+from bleach import Cleaner, sanitizer
+from config import ALLOWED_HTML_BODY_TAGS, ATTACHMENTS_TO_STORE
+from mail import Attachment, Email
 
 
 class PublishService:
@@ -26,9 +26,11 @@ class PublishService:
             my_gobits = [metadata.to_json()]
         except:  # noqa: E722
             my_gobits = []
-        message_to_publish = {'gobits': my_gobits, message_name: message}
+        message_to_publish = {"gobits": my_gobits, message_name: message}
         print(json.dumps(message_to_publish))
-        self._publisher.publish(self._topic_name, bytes(json.dumps(message_to_publish).encode('utf-8')))
+        self._publisher.publish(
+            self._topic_name, bytes(json.dumps(message_to_publish).encode("utf-8"))
+        )
 
 
 class MailPublishService(PublishService):
@@ -37,40 +39,45 @@ class MailPublishService(PublishService):
 
         if ATTACHMENTS_TO_STORE:
             return {
-                'sent_on': email.time_sent.isoformat(),
-                'received_on': email.time_received.isoformat(),
-                'subject': self.parse_html_content(email.subject, tags=[]),
-                'sender': email.sender,
-                'recipient': email.receiver,
-                'body': self.parse_html_content(email.body, tags=allowed_html_body_tags),
-                'attachments': [self._convert_attachment_to_message(attachment) for attachment in email.attachments
-                                if attachment.content_type in ATTACHMENTS_TO_STORE]
+                "sent_on": email.time_sent.isoformat(),
+                "received_on": email.time_received.isoformat(),
+                "subject": self.parse_html_content(email.subject, tags=[]),
+                "sender": email.sender,
+                "recipient": email.receiver,
+                "body": self.parse_html_content(
+                    email.body, tags=allowed_html_body_tags
+                ),
+                "attachments": [
+                    self._convert_attachment_to_message(attachment)
+                    for attachment in email.attachments
+                    if attachment.content_type in ATTACHMENTS_TO_STORE
+                ],
             }
 
         return {
-                'sent_on': email.time_sent.isoformat(),
-                'received_on': email.time_received.isoformat(),
-                'subject': self.parse_html_content(email.subject, tags=[]),
-                'sender': email.sender,
-                'recipient': email.receiver,
-                'body': self.parse_html_content(email.body, tags=allowed_html_body_tags),
-                'attachments': []
-            }
+            "sent_on": email.time_sent.isoformat(),
+            "received_on": email.time_received.isoformat(),
+            "subject": self.parse_html_content(email.subject, tags=[]),
+            "sender": email.sender,
+            "recipient": email.receiver,
+            "body": self.parse_html_content(email.body, tags=allowed_html_body_tags),
+            "attachments": [],
+        }
 
     def _convert_attachment_to_message(self, attachment: Attachment):
         return {
-            'mimetype': attachment.content_type,
-            'bucket': attachment.storage_bucket,
-            'file_name': attachment.name,
-            'full_path': attachment.storage_filename
+            "mimetype": attachment.content_type,
+            "bucket": attachment.storage_bucket,
+            "file_name": attachment.name,
+            "full_path": attachment.storage_filename,
         }
 
     def publish_email(self, email: Email):
         message = self._convert_email_to_message(email)
 
-        self._publish_message('email', message)
+        self._publish_message("email", message)
 
-        logging.info('Published message for email {}'.format(email.uuid))
+        logging.info("Published message for email {}".format(email.uuid))
 
     def parse_html_content(self, html, **kwargs):
         if html is None:
--- functions/ews-mail-ingest/utils.py
+++ functions/ews-mail-ingest/utils.py
@@ -9,7 +9,8 @@ def get_secret(project_id, secret_id):
     client = secretmanager.SecretManagerServiceClient()
 
     secret_name = client.access_secret_version(
-                  request={"name": f"projects/{project_id}/secrets/{secret_id}/versions/latest"})
+        request={"name": f"projects/{project_id}/secrets/{secret_id}/versions/latest"}
+    )
 
     payload = secret_name.payload.data.decode("UTF-8")
 
